8c50224774fbbd54848636a8e17d55e9fa4798df
wangzhuowei <wangzhuowei@bytedance.com>
2021-09-26 17:34:32 +0800

    Revert "feat: redefine EventLoop.Serve arg:  `Listener` -> `net.Listener` (#29)"
    This reverts commit eb71b1f3fe4a8a096924d5a1d8f5a8f0c8a6e9c0.

--- net_listener.go
+++ net_listener.go
@@ -17,7 +17,6 @@
 package netpoll
 
 import (
-	"errors"
 	"net"
 	"os"
 	"syscall"
@@ -33,44 +32,41 @@ type Listener interface {
 
 // CreateListener return a new Listener.
 func CreateListener(network, addr string) (l Listener, err error) {
-	if network == "udp" {
+	ln := &listener{
+		network: network,
+		addr:    addr,
+	}
+
+	defer func() {
+		if err != nil {
+			ln.Close()
+		}
+	}()
+
+	if ln.network == "udp" {
 		// TODO: udp listener.
-		return udpListener(network, addr)
-	}
-	// tcp, tcp4, tcp6, unix
-	ln, err := net.Listen(network, addr)
-	if err != nil {
-		return nil, err
-	}
-	return ConvertListener(ln)
-}
-
-// ConvertListener converts net.Listener to Listener
-func ConvertListener(l net.Listener) (nl Listener, err error) {
-	if tmp, ok := l.(Listener); ok {
-		return tmp, nil
-	}
-	ln := &listener{}
-	ln.ln = l
-	ln.addr = l.Addr()
-	err = ln.parseFD()
-	if err != nil {
-		return nil, err
-	}
-	return ln, syscall.SetNonblock(ln.fd, true)
-}
-
-// TODO: udpListener does not work now.
-func udpListener(network, addr string) (l Listener, err error) {
-	ln := &listener{}
-	ln.pconn, err = net.ListenPacket(network, addr)
-	if err != nil {
-		return nil, err
-	}
-	ln.addr = ln.pconn.LocalAddr()
-	switch pconn := ln.pconn.(type) {
-	case *net.UDPConn:
-		ln.file, err = pconn.File()
+		ln.pconn, err = net.ListenPacket(ln.network, ln.addr)
+		if err != nil {
+			return nil, err
+		}
+		ln.lnaddr = ln.pconn.LocalAddr()
+		switch pconn := ln.pconn.(type) {
+		case *net.UDPConn:
+			ln.file, err = pconn.File()
+		}
+	} else {
+		// tcp, tcp4, tcp6, unix
+		ln.ln, err = net.Listen(ln.network, ln.addr)
+		if err != nil {
+			return nil, err
+		}
+		ln.lnaddr = ln.ln.Addr()
+		switch netln := ln.ln.(type) {
+		case *net.TCPListener:
+			ln.file, err = netln.File()
+		case *net.UnixListener:
+			ln.file, err = netln.File()
+		}
 	}
 	if err != nil {
 		return nil, err
@@ -82,11 +78,13 @@ func udpListener(network, addr string) (l Listener, err error) {
 var _ net.Listener = &listener{}
 
 type listener struct {
-	fd    int
-	addr  net.Addr       // listener's local addr
-	ln    net.Listener   // tcp|unix listener
-	pconn net.PacketConn // udp listener
-	file  *os.File
+	ln      net.Listener   // tcp|unix listener
+	lnaddr  net.Addr       // listener's local addr
+	pconn   net.PacketConn // udp listener
+	file    *os.File
+	fd      int
+	network string // tcp, tcp4, tcp6, udp, udp4, udp6, unix
+	addr    string
 }
 
 // Accept implements Listener.
@@ -105,8 +103,8 @@ func (ln *listener) Accept() (net.Conn, error) {
 	}
 	var nfd = &netFD{}
 	nfd.fd = fd
-	nfd.localAddr = ln.addr
-	nfd.network = ln.addr.Network()
+	nfd.network = ln.network
+	nfd.localAddr = ln.lnaddr
 	nfd.remoteAddr = sockaddrToAddr(sa)
 	return nfd, nil
 }
@@ -130,31 +128,18 @@ func (ln *listener) Close() error {
 	if ln.pconn != nil {
 		ln.pconn.Close()
 	}
+	if ln.network == "unix" {
+		os.RemoveAll(ln.addr)
+	}
 	return nil
 }
 
 // Addr implements Listener.
 func (ln *listener) Addr() net.Addr {
-	return ln.addr
+	return ln.lnaddr
 }
 
 // Fd implements Listener.
 func (ln *listener) Fd() (fd int) {
 	return ln.fd
-}
-
-func (ln *listener) parseFD() (err error) {
-	switch netln := ln.ln.(type) {
-	case *net.TCPListener:
-		ln.file, err = netln.File()
-	case *net.UnixListener:
-		ln.file, err = netln.File()
-	default:
-		return errors.New("listener type can't support")
-	}
-	if err != nil {
-		return err
-	}
-	ln.fd = int(ln.file.Fd())
-	return nil
 }
--- netpoll.go
+++ netpoll.go
@@ -18,7 +18,6 @@ package netpoll
 
 import (
 	"context"
-	"net"
 	"sync"
 )
 
@@ -27,7 +26,7 @@ type EventLoop interface {
 	// Serve registers a listener and runs blockingly to provide services, including listening to ports,
 	// accepting connections and processing trans data. When an exception occurs or Shutdown is invoked,
 	// Serve will return an error which describes the specific reason.
-	Serve(ln net.Listener) error
+	Serve(ln Listener) error
 
 	// Shutdown is used to graceful exit.
 	// It will close all idle connections on the server, but will not change the underlying pollers.
@@ -102,13 +101,9 @@ type eventLoop struct {
 }
 
 // Serve implements EventLoop.
-func (evl *eventLoop) Serve(ln net.Listener) error {
-	npln, err := ConvertListener(ln)
-	if err != nil {
-		return err
-	}
+func (evl *eventLoop) Serve(ln Listener) error {
 	evl.Lock()
-	evl.svr = newServer(npln, evl.prepare, evl.quit)
+	evl.svr = newServer(ln, evl.prepare, evl.quit)
 	evl.svr.Run()
 	evl.Unlock()
 	return evl.waitQuit()
