874be5d3bace1c48ecc548e275f4addb0c8fa65f
wangzhuowei <wangzhuowei@bytedance.com>
2021-11-29 18:21:57 +0800

    opt

--- poll_default_linux.go
+++ poll_default_linux.go
@@ -105,9 +105,9 @@ func (p *defaultPoll) Wait() (err error) {
 	p.Reset(128, barriercap)
 	var n, miss, msec int
 	for {
-		if miss >= 1 && miss <= 5 {
+		if miss == 1 {
 			runtime.Gosched()
-		} else if miss > 5 {
+		} else if miss > 1 {
 			msec = -1
 		}
 
